aac485a1efb17768058388beb51c6cdc7bafe8a3
bb-splunk <bbetka@splunk.com>
2023-10-19 16:58:04 +0200

    Removed requests, six and indirect requirements from requirements.txt

--- awswafv2_connector.py
+++ awswafv2_connector.py
@@ -22,7 +22,6 @@ import botocore.paginate as bp
 import botocore.response as br
 import phantom.app as phantom
 import requests
-import six
 from boto3 import Session, client
 from botocore.config import Config
 from phantom.action_result import ActionResult
@@ -64,7 +63,7 @@ class AwsWafConnector(BaseConnector):
 
         if isinstance(cur_obj, dict):
             new_dict = {}
-            for k, v in six.iteritems(cur_obj):
+            for k, v in cur_obj.items():
                 if isinstance(v, br.StreamingBody):
                     content = v.read()
                     new_dict[k] = json.loads(content)
